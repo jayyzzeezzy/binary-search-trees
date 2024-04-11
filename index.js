043c359d0b1a86ff4eec8448b90e479206835d5c
James Zhou <jam9es@gmail.com>
2024-04-10 17:52:04 -0700

    Write driver scripts to test out codes

--- index.js
+++ index.js
@@ -217,3 +217,28 @@ class Tree {
         this.root = this.buildTree(sorted);
     };
 };
+
+
+// driver script is below:
+const randomArray = (size) => {
+    return Array.from({ length: size }, () => Math.floor(Math.random() * 100));
+};
+
+const tree = new Tree(randomArray(15));
+console.log('Balanced:', tree.isBalanced());
+console.log('Lever Order =>', tree.levelOrder());
+console.log('Preorder =>', tree.preOrder());
+console.log('Inorder =>', tree.inOrder());
+console.log('Postorder =>', tree.postOrder());
+
+for (let i = 0; i < 5; i++) {
+  tree.insert(Math.floor(Math.random() * 20));
+}
+console.log('Balanced:', tree.isBalanced());
+
+tree.rebalance();
+console.log('Balanced:', tree.isBalanced());
+console.log('Lever Order =>', tree.levelOrder());
+console.log('Preorder =>', tree.preOrder());
+console.log('In-order =>', tree.inOrder());
+console.log('Post-order =>', tree.postOrder());
